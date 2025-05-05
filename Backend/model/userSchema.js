d2f06cdf62bd8ffc3edefa4bef2cd4ac471c8acf
Abhi <abhivignesh27@gmail.com>
2025-05-05 15:33:58 +0530

    done

--- Backend/model/userSchema.js
+++ Backend/model/userSchema.js
@@ -1,28 +1,28 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 
-const Userschema =new mongoose.Schema({
-    name:{
-        type:String,
+const Userschema = new mongoose.Schema({
+    name: {
+        type: String,
         // required:true
     },
-    email:{
-        type:String,
-        required:true
+    email: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    role:{
-        type:String,
-        default:"user",
-        enum:["user","admin"]
+    role: {
+        type: String,
+        default: "user",
+        enum: ["user", "admin"]
     }
-   
+
 
 })
 
 
-const UserModel =mongoose.model("user",Userschema)
+const UserModel = mongoose.model("user", Userschema)
 
-module.exports={UserModel}
+module.exports = { UserModel }
